fc4e7c7bb8b9f26dc5f8b550736de47ebf616d28
zacanger <zac@zacanger.com>
2016-07-16 21:41:10 -0600

    version bump

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-module.exports = function log(){
+function log () {
   if (doLog && process.env.NODE_ENV !== 'production' && console) {
     console.log(...arguments)
   }
 }
+
+module.exports = log
